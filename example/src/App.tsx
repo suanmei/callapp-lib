d03cb54553f7b3a0fa42a7113ae6162fd7add2b2
zhaolei <lei40251@163.com>
2022-06-20 12:56:26 +0800

    fix: 修复 append 在IE浏览器的兼容问题

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -46,7 +46,7 @@ function evokeByTagA(uri: string): void {
 
   tagA.setAttribute('href', uri);
   tagA.style.display = 'none';
-  document.body.append(tagA);
+  document.body.appendChild(tagA);
 
   tagA.click();
 }
